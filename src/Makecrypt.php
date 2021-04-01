8a38ffebc0fde46d30eec72a18df00b657e9e778
Jayesh <jayesh.bhagat@kissht.com>
2021-04-01 15:16:15 +0530

    Test package laravel

---
+++ src/Makecrypt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kisshtcrypt\Makecrypt;
+
+class Makecrypt
+{
+    public function crypt(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
